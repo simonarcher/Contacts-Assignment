2da888c2e7fffcb37a2de772640446e236f2d5c0
Simon Archer <simonarch@gmail.com>
2017-07-25 18:46:51 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Simon Archer on 2017/07/25.
+//  Copyright © 2017 Simon Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
