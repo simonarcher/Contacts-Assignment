1a6299f7a9efbfd5f05851a1826542c12aa0807d
Simon Archer <simonarch@gmail.com>
2017-07-25 19:19:19 +0200

    added icons and initial navigation

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contactsTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        contactsTableView.delegate = self
+        contactsTableView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "" {
+            print("Preparing segue for AddContactSegue")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addContactButtonPressed(_ sender: UIBarButtonItem) {
+        print("addContactButtonPressed")
+        
+        performSegue(withIdentifier: "AddContactSegue", sender: nil)
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
